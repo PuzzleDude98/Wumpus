e18d7c4279a3631848863ff656dc39b8cdb0be2b
PuzzleDude98 <puzzledude98@outlook.com>
2023-04-25 13:25:22 -0700

    Triangle Button Test

--- Basic GUI 1/Basic GUI 1/Form1.cs
+++ Basic GUI 1/Basic GUI 1/Form1.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +17,15 @@ namespace Basic_GUI_1
         public Form1()
         {
             InitializeComponent();
+            var path = new GraphicsPath();
+            Point[] pTPoints = { new Point(30, 0), new Point(60, 60), new Point(0, 60) };
+            path.AddLines(pTPoints);
+            button1.Region = new Region(path);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            notifyIcon1.ShowBalloonTip(5000);
         }
     }
 }
