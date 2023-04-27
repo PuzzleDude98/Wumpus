90c2b036f0423b974c25532a2a3da2c3e16b7122
PuzzleDude98 <puzzledude98@outlook.com>
2023-04-27 12:47:56 -0700

    Implement movement buttons

--- Basic GUI 1/Basic GUI 1/Form1.Designer.cs
+++ Basic GUI 1/Basic GUI 1/Form1.Designer.cs
@@ -31,8 +31,13 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
-            this.button1 = new System.Windows.Forms.Button();
+            this.buttonNE = new System.Windows.Forms.Button();
             this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.buttonE = new System.Windows.Forms.Button();
+            this.buttonSE = new System.Windows.Forms.Button();
+            this.buttonSW = new System.Windows.Forms.Button();
+            this.buttonW = new System.Windows.Forms.Button();
+            this.buttonNW = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // notifyIcon1
@@ -43,24 +48,93 @@
             this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
             this.notifyIcon1.Visible = true;
             // 
-            // button1
+            // buttonNE
             // 
-            this.button1.Location = new System.Drawing.Point(348, 156);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.buttonNE.BackColor = System.Drawing.Color.Transparent;
+            this.buttonNE.FlatAppearance.BorderSize = 0;
+            this.buttonNE.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonNE.Location = new System.Drawing.Point(470, 0);
+            this.buttonNE.Name = "buttonNE";
+            this.buttonNE.Size = new System.Drawing.Size(220, 250);
+            this.buttonNE.TabIndex = 0;
+            this.buttonNE.Text = "North East";
+            this.buttonNE.UseVisualStyleBackColor = false;
+            // 
+            // buttonE
+            // 
+            this.buttonE.BackColor = System.Drawing.Color.Transparent;
+            this.buttonE.FlatAppearance.BorderSize = 0;
+            this.buttonE.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonE.Location = new System.Drawing.Point(470, 125);
+            this.buttonE.Name = "buttonE";
+            this.buttonE.Size = new System.Drawing.Size(220, 250);
+            this.buttonE.TabIndex = 1;
+            this.buttonE.Text = "East";
+            this.buttonE.UseVisualStyleBackColor = false;
+            // 
+            // buttonSE
+            // 
+            this.buttonSE.BackColor = System.Drawing.Color.Transparent;
+            this.buttonSE.FlatAppearance.BorderSize = 0;
+            this.buttonSE.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSE.Location = new System.Drawing.Point(470, 250);
+            this.buttonSE.Name = "buttonSE";
+            this.buttonSE.Size = new System.Drawing.Size(220, 250);
+            this.buttonSE.TabIndex = 2;
+            this.buttonSE.Text = "South East";
+            this.buttonSE.UseVisualStyleBackColor = false;
+            // 
+            // buttonSW
+            // 
+            this.buttonSW.BackColor = System.Drawing.Color.Transparent;
+            this.buttonSW.FlatAppearance.BorderSize = 0;
+            this.buttonSW.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSW.Location = new System.Drawing.Point(250, 250);
+            this.buttonSW.Name = "buttonSW";
+            this.buttonSW.Size = new System.Drawing.Size(220, 250);
+            this.buttonSW.TabIndex = 5;
+            this.buttonSW.Text = "South West";
+            this.buttonSW.UseVisualStyleBackColor = false;
+            // 
+            // buttonW
+            // 
+            this.buttonW.BackColor = System.Drawing.Color.Transparent;
+            this.buttonW.FlatAppearance.BorderSize = 0;
+            this.buttonW.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonW.Location = new System.Drawing.Point(250, 125);
+            this.buttonW.Name = "buttonW";
+            this.buttonW.Size = new System.Drawing.Size(220, 250);
+            this.buttonW.TabIndex = 4;
+            this.buttonW.Text = "West";
+            this.buttonW.UseVisualStyleBackColor = false;
+            // 
+            // buttonNW
+            // 
+            this.buttonNW.BackColor = System.Drawing.Color.Transparent;
+            this.buttonNW.FlatAppearance.BorderSize = 0;
+            this.buttonNW.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonNW.Location = new System.Drawing.Point(250, 0);
+            this.buttonNW.Name = "buttonNW";
+            this.buttonNW.Size = new System.Drawing.Size(220, 250);
+            this.buttonNW.TabIndex = 3;
+            this.buttonNW.Text = "North West";
+            this.buttonNW.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(944, 501);
+            this.Controls.Add(this.buttonSW);
+            this.Controls.Add(this.buttonW);
+            this.Controls.Add(this.buttonNW);
+            this.Controls.Add(this.buttonSE);
+            this.Controls.Add(this.buttonE);
+            this.Controls.Add(this.buttonNE);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "GUI";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.ResumeLayout(false);
 
         }
@@ -68,8 +142,13 @@
         #endregion
 
         private System.Windows.Forms.NotifyIcon notifyIcon1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonNE;
         private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button buttonE;
+        private System.Windows.Forms.Button buttonSE;
+        private System.Windows.Forms.Button buttonSW;
+        private System.Windows.Forms.Button buttonW;
+        private System.Windows.Forms.Button buttonNW;
     }
 }
 
--- Basic GUI 1/Basic GUI 1/Form1.cs
+++ Basic GUI 1/Basic GUI 1/Form1.cs
@@ -17,15 +17,43 @@ namespace Basic_GUI_1
         public Form1()
         {
             InitializeComponent();
-            var path = new GraphicsPath();
-            Point[] pTPoints = { new Point(30, 0), new Point(60, 60), new Point(0, 60) };
-            path.AddLines(pTPoints);
-            button1.Region = new Region(path);
+
+            var pathne = new GraphicsPath();
+            Point[] pTPointsne = { new Point(0, 0), new Point(220, 125), new Point(0,270) };
+            pathne.AddLines(pTPointsne);
+            buttonNE.Region = new Region(pathne);
+
+            var pathe = new GraphicsPath();
+            Point[] pTPointe = { new Point(0, 125), new Point(220, 0), new Point(220, 250) };
+            pathe.AddLines(pTPointe);
+            buttonE.Region = new Region(pathe);
+
+            var pathse = new GraphicsPath();
+            Point[] pTPointse = { new Point(0, 0), new Point(0, 250), new Point(220, 125) };
+            pathse.AddLines(pTPointse);
+            buttonSE.Region = new Region(pathse);
+
+            var pathnw = new GraphicsPath();
+            Point[] pTPointsnw = { new Point(220, 0), new Point(220, 250), new Point(0, 125) };
+            pathnw.AddLines(pTPointsnw);
+            buttonNW.Region = new Region(pathnw);
+
+            var pathw = new GraphicsPath();
+            Point[] pTPointw = { new Point(0, 0), new Point(0, 250), new Point(220, 125) };
+            pathw.AddLines(pTPointw);
+            buttonW.Region = new Region(pathw);
+
+            var pathsw = new GraphicsPath();
+            Point[] pTPointsw= { new Point(0, 125), new Point(220, 00), new Point(220, 250) };
+            pathsw.AddLines(pTPointsw);
+            buttonSW.Region = new Region(pathsw);
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            notifyIcon1.ShowBalloonTip(5000);
+            Point[] HexPoints = { new Point(470, 0), new Point(690, 125), new Point(690, 375), new Point(470, 500), new Point(250, 375), new Point(250, 125) };
+            Pen blackpen = new Pen(Color.Black,2);
+            e.Graphics.DrawPolygon(blackpen, HexPoints);
         }
     }
 }
